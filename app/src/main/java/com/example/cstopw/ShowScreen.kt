1fabf51b97067a202fad8db6dca86046a60e5ba6
W L PROJECT <rickyvishwas11@gmail.com>
2025-05-08 11:49:44 +0530

    permission update

--- app/src/main/java/com/example/cstopw/ShowScreen.kt
+++ app/src/main/java/com/example/cstopw/ShowScreen.kt
@@ -2,6 +2,9 @@ package com.example.cstopw
 
 import android.Manifest
 import android.content.res.Configuration
+import android.os.Build
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.annotation.RequiresPermission
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
@@ -9,6 +12,9 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -20,6 +26,24 @@ import androidx.compose.ui.unit.sp
 @Composable
 fun ShowScreen(service: StopwatchService?) {
     val configuration = LocalConfiguration.current
+
+    val onPermission = remember { mutableStateOf(false) }
+
+    val permissionLauncher = rememberLauncherForActivityResult(
+        contract = ActivityResultContracts.RequestPermission(),
+        onResult = {
+            onPermission.value = it
+        }
+    )
+
+    LaunchedEffect(!onPermission.value) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            permissionLauncher.launch(Manifest.permission.POST_NOTIFICATIONS)
+        } else {
+            onPermission.value = true
+        }
+    }
+
     when(configuration.orientation) {
         Configuration.ORIENTATION_LANDSCAPE -> {
             MainScreenRotate(service = service)
